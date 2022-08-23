1666077427eac675dc418e508ae0b979a4b69d72
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2022-08-22 20:02:06 -0500

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import "./mylabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Label comentario
+   */
+  label: string;
+  /**
+   * AllCaps en false
+   */
+  allCaps?: boolean;
+  /**
+   * Primary, Secondary o Tertiary
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * Size comentario
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Color de fuente personalizado
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color = "primary",
+  label = "no label",
+  size = "normal",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: `${fontColor}` }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
